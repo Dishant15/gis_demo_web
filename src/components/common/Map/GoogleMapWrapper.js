f0ce58f71759de7f63e808de0235ba5309d9eaa8
Harsh Darji <harshdarji1995@gmail.com>
2022-08-06 23:19:40 +0530

    added new marker icons

--- src/components/common/Map/GoogleMapWrapper.js
+++ src/components/common/Map/GoogleMapWrapper.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { GOOGLE_MAP_KEY, MAP_LIBRARIES } from "./map.constants";
+import { PIN_P, SQUARE_P } from "./MarkerPaths";
 
 const DEFAULT_CONTAINER_STYLE = {
   width: "100%",
@@ -61,6 +62,44 @@ export const RedMarker = (props) => {
       {...props}
       icon={{
         path: "M25 0c-8.284 0-15 6.656-15 14.866 0 8.211 15 35.135 15 35.135s15-26.924 15-35.135C40 6.656 33.284 0 25 0zm-.049 19.312c-2.557 0-4.629-2.055-4.629-4.588 0-2.535 2.072-4.589 4.629-4.589 2.559 0 4.631 2.054 4.631 4.589 0 2.533-2.072 4.588-4.631 4.588z",
+        fillColor: "#d85b53",
+        fillOpacity: 1,
+        strokeColor: "white",
+        strokeOpacity: 1,
+        strokeWeight: 1,
+        scale: 0.8,
+        anchor: new window.google.maps.Point(26, 50),
+        labelOrigin: new window.google.maps.Point(26, 50),
+      }}
+    />
+  );
+};
+
+export const RedMarker1 = (props) => {
+  return (
+    <Marker
+      {...props}
+      icon={{
+        path: PIN_P,
+        fillColor: "#d85b53",
+        fillOpacity: 1,
+        strokeColor: "white",
+        strokeOpacity: 1,
+        strokeWeight: 1,
+        scale: 0.8,
+        anchor: new window.google.maps.Point(26, 50),
+        labelOrigin: new window.google.maps.Point(26, 50),
+      }}
+    />
+  );
+};
+
+export const RedMarker2 = (props) => {
+  return (
+    <Marker
+      {...props}
+      icon={{
+        path: SQUARE_P,
         fillColor: "#d85b53",
         fillOpacity: 1,
         strokeColor: "white",
