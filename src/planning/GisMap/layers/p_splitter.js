d5a6db0e5c5c0d21f50f979721a3b1706e9225c5
Harsh Darji <harshdarji1995@gmail.com>
2022-08-15 17:13:42 +0530

    added spliter icon

--- src/planning/GisMap/layers/p_splitter.js
+++ src/planning/GisMap/layers/p_splitter.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Marker } from "@react-google-maps/api";
 
-import PDPViewIcon from "assets/markers/p_dp_view.svg";
+import SPLITER_ICON from "assets/markers/spliter_view.svg";
 import { getLayerViewData } from "planning/data/planningGis.selectors";
 
 export const LAYER_KEY = "p_splitter";
@@ -20,7 +20,11 @@ export const ViewLayer = () => {
       {data.map((dp) => {
         const { id, coordinates } = dp;
         return (
-          <Marker icon={{ url: PDPViewIcon }} key={id} position={coordinates} />
+          <Marker
+            icon={{ url: SPLITER_ICON }}
+            key={id}
+            position={coordinates}
+          />
         );
       })}
     </>
