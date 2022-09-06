8ac2139419747c2f573060ce3e24e4432286c96c
Harsh Darji <harshdarji1995@gmail.com>
2022-09-06 13:45:15 +0530

    updated svg icons

--- src/planning/GisMap/layers/p_splitter/ViewLayer.js
+++ src/planning/GisMap/layers/p_splitter/ViewLayer.js
@@ -2,7 +2,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Marker } from "@react-google-maps/api";
 
-import SPLITER_ICON from "assets/markers/spliter_view.svg";
+import PrimarySpliterIcon from "assets/markers/spliter_view_primary.svg";
+import SecondarySpliterIcon from "assets/markers/spliter_view.svg";
+
 import { getLayerViewData } from "planning/data/planningGis.selectors";
 
 export const LAYER_KEY = "p_splitter";
@@ -18,10 +20,15 @@ export const ViewLayer = () => {
   return (
     <>
       {data.map((dp) => {
-        const { id, coordinates } = dp;
+        const { id, coordinates, splitter_type } = dp;
         return (
           <Marker
-            icon={{ url: SPLITER_ICON }}
+            icon={{
+              url:
+                splitter_type === "P"
+                  ? PrimarySpliterIcon
+                  : SecondarySpliterIcon,
+            }}
             key={id}
             position={coordinates}
           />
