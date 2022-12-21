f09643ea13a0352b656f8c75a64e193afeb4d126
Harsh Darji <harshdarji1995@gmail.com>
2022-12-21 12:28:21 +0530

    move datepicker to popover

--- src/components/common/FormFields/FormDatePicker.js
+++ src/components/common/FormFields/FormDatePicker.js
@@ -1,8 +1,12 @@
-import React from "react";
-import { TextField } from "@mui/material";
+import React, { useCallback, useState } from "react";
 import { Controller } from "react-hook-form";
 
-import { DatePickerInput } from "react-datetime-range-super-picker";
+import Popover from "@mui/material/Popover";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+
+import { DatePicker } from "react-datetime-range-super-picker";
+import { format } from "date-fns";
 
 export const FormDatePicker = ({
   label,
@@ -14,25 +18,61 @@ export const FormDatePicker = ({
   helperText,
   ...rest
 }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = useCallback((event) => {
+    setAnchorEl(event.target);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setAnchorEl(null);
+  }, []);
+
+  const open = Boolean(anchorEl);
+
   return (
     <Controller
       render={({ field }) => {
         return (
-          <DatePickerInput
-            date={field.value || undefined}
-            onDateUpdate={({ date }) => field.onChange(date)}
-            inputComponent={
+          <>
+            <Box onClick={handleClick}>
               <TextField
+                className="full-width"
                 InputLabelProps={{
                   required: required,
+                  shrink: !!field.value,
                 }}
                 error={errors}
                 label={label}
                 helperText={helperText}
+                value={field.value ? format(field.value, "dd/MM/YYY") : null}
               />
-            }
-            {...rest}
-          />
+            </Box>
+            <Popover
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              transformOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              disableAutoFocus
+              disableEnforceFocus
+            >
+              <DatePicker
+                date={field.value || undefined}
+                onDateUpdate={({ date }) => {
+                  field.onChange(date);
+                  handleClose();
+                }}
+                {...rest}
+              />
+            </Popover>
+          </>
         );
       }}
       name={name}
