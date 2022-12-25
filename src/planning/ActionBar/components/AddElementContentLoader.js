44bad64115e1c082e3bef65fe69bd2f49859a4ee
Harsh Darji <harshdarji1995@gmail.com>
2022-12-25 11:09:01 +0530

    loader update

---
+++ src/planning/ActionBar/components/AddElementContentLoader.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddElementContentLoader = () => {
+  return <div>AddElementContentLoader</div>;
+};
+
+export default AddElementContentLoader;
--- src/planning/GisMap/components/GisMapPopups/GisMapPopupLoader.js
+++ src/planning/GisMap/components/GisMapPopups/GisMapPopupLoader.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+import range from "lodash/range";
+
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Skeleton from "@mui/material/Skeleton";
-
-import range from "lodash/range";
+import Divider from "@mui/material/Divider";
 
 import GisMapPopups from "planning/GisMap/components/GisMapPopups";
 
@@ -51,4 +52,36 @@ const GisMapPopupLoader = () => {
   );
 };
 
+export const GisElementTableLoader = () => {
+  const rowPills = range(6);
+
+  return (
+    <GisMapPopups>
+      <Box minWidth="350px" maxWidth="550px">
+        <Stack
+          sx={{ backgroundColor: "primary.main", color: "background.default" }}
+          direction="row"
+          alignItems="center"
+          p={1}
+        >
+          <Typography variant="h6" flex={1}>
+            <Skeleton />
+          </Typography>
+        </Stack>
+        <Stack divider={<Divider />}></Stack>
+        {rowPills.map((ind) => {
+          return (
+            <Stack direction="row" key={ind} px={2} py={1}>
+              <Skeleton
+                height="40px"
+                width="100%"
+                sx={{ transform: "unset" }}
+              />
+            </Stack>
+          );
+        })}
+      </Box>
+    </GisMapPopups>
+  );
+};
 export default GisMapPopupLoader;
