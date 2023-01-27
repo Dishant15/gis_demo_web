6c40e1fc26079119a0585570502deda0025b9bad
Dishant Chavda <dishantchavda@gmail.com>
2023-01-27 15:26:44 +0530

    dp configuration updates

--- src/planning/GisMap/layers/p_dp/configurations.js
+++ src/planning/GisMap/layers/p_dp/configurations.js
@@ -6,6 +6,7 @@ import {
 
 import { default as Icon } from "assets/markers/p_dp_view.svg";
 import { default as EditIcon } from "assets/markers/p_dp_edit.svg";
+import { FIELD_TYPES } from "components/common/DynamicForm";
 
 export const LAYER_KEY = "p_dp";
 export const PRE_UID = "DP";
@@ -16,9 +17,24 @@ export const getViewOptions = () => ({
   pin: EditIcon,
 });
 
+export const DP_TYPE_OPTIONS = [
+  { value: "H", label: "FTTH" },
+  { value: "B", label: "FTTB" },
+];
+
+export const CABINET_TYPE_OPTIONS = [
+  { value: "FA", label: "FAT" },
+  { value: "JC", label: "Joint Closer" },
+  { value: "CB", label: "Compass Box" },
+  { value: "MB", label: "Metal Box" },
+  { value: "FD", label: "FDC" },
+];
+
 export const INITIAL_ELEMENT_DATA = {
   name: "",
   address: "",
+  dp_type: "H",
+  cabinet_type: "FA",
   unique_id: "",
   network_id: "",
   ref_code: "",
@@ -36,7 +52,19 @@ export const ELEMENT_FORM_TEMPLATE = {
         {
           field_key: "address",
           label: "Address",
-          field_type: "textArea",
+          field_type: FIELD_TYPES.TextArea,
+        },
+        {
+          field_key: "dp_type",
+          label: "Dp Type",
+          field_type: FIELD_TYPES.Select,
+          options: DP_TYPE_OPTIONS,
+        },
+        {
+          field_key: "cabinet_type",
+          label: "Cabinet Type",
+          field_type: FIELD_TYPES.Select,
+          options: CABINET_TYPE_OPTIONS,
         },
       ],
     },
@@ -52,10 +80,16 @@ export const ELEMENT_FORM_TEMPLATE = {
 export const ELEMENT_TABLE_FIELDS = [
   ...ELEMENT_TABLE_ABSTRACT_FIELDS,
   { label: "Address", field: "address", type: "simple" },
+  { label: "Dp Type", field: "dp_type_display", type: "simple" },
+  { label: "Cabinet Type", field: "cabinet_type_display", type: "simple" },
 ];
 
 export const ELEMENT_TABLE_EXTRA_CONTROLS = [
   {
-    control: "connections",
+    control: "add_associations",
+    data: ["p_splitter"],
+  },
+  {
+    control: "association_list",
   },
 ];
